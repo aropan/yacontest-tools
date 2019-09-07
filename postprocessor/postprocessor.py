a1bc35ef6fe8326317f6ed68a7065fe985824d50
Aleksey Ropan <nap0rbl4@gmail.com>
2019-09-07 14:11:02 +0300

    Add postprocessor

---
+++ postprocessor/postprocessor.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import simplejson as json
+from sys import stdin, stderr, stdout
+import traceback
+
+
+def main():
+    test_data = json.load(stdin)['tests']
+    with open('scoring.json', 'r') as fd:
+        scoring_data = json.load(fd)
+
+    important = ['testName', 'verdict', 'message', 'runningTime', 'memoryUsed']
+    tests = [{field: test.get(field) for field in important} for test in test_data if test['testName'] != 'samples']
+    passed = [test['verdict'] == 'ok' for test in tests]
+
+    total_points = 0
+    for scoring in scoring_data['scoring']:
+        points = scoring['points']
+        required_passed = True
+        for_each_test = bool(scoring.get('each'))
+
+        for required_test in scoring.get('required', {}).get('tests'):
+            if isinstance(required_test, int):
+                tests = [required_test]
+            elif isinstance(required_test, str) and '..' in required_test:
+                a, b = map(int, required_test.split('..'))
+                tests = range(a, b + 1)
+            else:
+                raise NotImplementedError('Unknown required_test="%s" format' % required_test)
+
+            for t in tests:
+                if not passed[t - 1]:
+                    required_passed = False
+                elif for_each_test:
+                    total_points += points
+
+        if required_passed and not for_each_test:
+            total_points += points
+
+    if abs(round(total_points) - total_points) < 1e-9:
+        stdout.write('%.0f\n' % total_points)
+    else:
+        stdout.write('%.2f\n' % total_points)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception:
+        stdout.write('-0\n')
+        traceback.print_exc(file=stderr)
